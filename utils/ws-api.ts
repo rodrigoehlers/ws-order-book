fe3b6e182603a7f89f4504bf4c0f44b7e0b2e51a
Rodrigo Ehlers <rodrigoehlers@outlook.com>
2021-05-19 10:33:59 +0200

    add grouping enum and unsubscribe payload helper

--- utils/ws-api.ts
+++ utils/ws-api.ts
@@ -1,3 +1,22 @@
+export enum Grouping {
+  POINT_FIVE = '1',
+  ONE = '2',
+  TWO_POINT_FIVE = '5',
+  FIVE = '10',
+  TEN = '20',
+  TWENTY_FIVE = '50',
+  FIFTY = '100',
+  ONE_HUNDRED = '200',
+  TWO_HUNDRED_AND_FIFTY = '500',
+  THOUSAND = '1000',
+  TWO_THOUSAND = '2000',
+  TWO_THOUSAND_FIVE_HUNDRED = '5000',
+}
+
 export const getSubscriptionRequestPayload = (feedId: string, productIds: string | string[]) => {
   return { event: 'subscribe', feed: feedId, product_ids: Array.isArray(productIds) ? productIds : [productIds] };
+};
+
+export const getSubscriptionCancelRequestPayload = (feedId: string, productIds: string | string[]) => {
+  return { event: 'unsubscribe', feed: feedId, product_ids: Array.isArray(productIds) ? productIds : [productIds] };
 };
