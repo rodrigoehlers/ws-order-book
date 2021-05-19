15a59ed1ccc50ab6ef513f75baaca29a3fb40ce3
Rodrigo Ehlers <rodrigoehlers@outlook.com>
2021-05-19 08:58:54 +0200

    use order book integration instead of direct ws

--- features/OrderBookFeature.tsx
+++ features/OrderBookFeature.tsx
@@ -1,35 +1,57 @@
-import { useEffect, useRef } from 'react';
-import { getSubscriptionRequestPayloadForProductIds } from '../utils/ws-api';
+import { useEffect, useRef, useState } from 'react';
+import CryptoFacilitiesOrderBook, { Asks, Bids, OrderBook, UpdateFunction } from '../integrations/order-book';
 
 const WSS_ENDPOINT = process.env.NEXT_PUBLIC_WSS_ENDPOINT;
 
 const OrderBookFeature = () => {
-  const wsRef = useRef<WebSocket>();
+  const orderBookRef = useRef<OrderBook>();
+
+  const [bids, setBids] = useState<Bids>([]);
+  const [asks, setAsks] = useState<Asks>([]);
+
+  const updateBidsAndAsks: UpdateFunction = (bids, asks) => {
+    setBids([...bids]);
+    setAsks([...asks]);
+  };
 
   useEffect(() => {
     // TODO: Handle `WSS_ENDPOINT` not defined.
-    const ws = new WebSocket(`wss://${WSS_ENDPOINT}`);
-    const requestPayload = getSubscriptionRequestPayloadForProductIds('PI_XBTUSD');
+    const orderBook = new CryptoFacilitiesOrderBook(WSS_ENDPOINT);
+    orderBookRef.current = orderBook;
 
-    // TODO: Send the subscription message after receiving the first `info` event.
-    ws.onopen = () => {
-      ws.send(JSON.stringify(requestPayload));
-    };
-
-    wsRef.current = ws;
+    const remove = orderBook.addUpdateListener(updateBidsAndAsks);
 
     return () => {
-      ws.close();
+      remove();
+      orderBook.close();
     };
   }, []);
 
   return (
     <div className="container mx-auto">
       <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">WS Order Book</h2>
+      <div className="flex">
+        <ul>
+          {bids.map((bid) => {
+            return (
+              <li key={bid[0]}>
+                {bid[0]} - {bid[1]}
+              </li>
+            );
+          })}
+        </ul>
+        <ul>
+          {asks.map((ask) => {
+            return (
+              <li key={ask[0]}>
+                {ask[0]} - {ask[1]}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default OrderBookFeature;
-
-// wss://
