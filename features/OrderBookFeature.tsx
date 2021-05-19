76f6c17c349e3561b4c976cf08faf6c6dc0da584
Rodrigo Ehlers <rodrigoehlers@outlook.com>
2021-05-19 10:35:14 +0200

    poc select and close ws on document hidden

--- features/OrderBookFeature.tsx
+++ features/OrderBookFeature.tsx
@@ -1,7 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import CryptoFacilitiesOrderBook, { Asks, Bids, OrderBook, UpdateFunction } from '../integrations/order-book';
+import { Grouping } from '../utils/ws-api';
 
 const WSS_ENDPOINT = process.env.NEXT_PUBLIC_WSS_ENDPOINT;
+const PRODUCT_IDS_RAW = process.env.NEXT_PUBLIC_PRODUCT_IDS;
+const PRODUCT_IDS = PRODUCT_IDS_RAW.split(',');
 
 const OrderBookFeature = () => {
   const orderBookRef = useRef<OrderBook>();
@@ -16,7 +19,7 @@ const OrderBookFeature = () => {
 
   useEffect(() => {
     // TODO: Handle `WSS_ENDPOINT` not defined.
-    const orderBook = new CryptoFacilitiesOrderBook(WSS_ENDPOINT);
+    const orderBook = new CryptoFacilitiesOrderBook(WSS_ENDPOINT, PRODUCT_IDS);
     orderBookRef.current = orderBook;
 
     const remove = orderBook.addUpdateListener(updateBidsAndAsks);
@@ -27,9 +30,43 @@ const OrderBookFeature = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (orderBookRef.current) {
+      const handleVisibilityChange = () => {
+        const { current: orderBook } = orderBookRef;
+        if (document.hidden) {
+          orderBook.close();
+        } else {
+          orderBook.open();
+        }
+      };
+
+      document.addEventListener('visibilitychange', handleVisibilityChange);
+
+      return () => {
+        document.removeEventListener('visibilitychange', handleVisibilityChange);
+      };
+    }
+  }, [orderBookRef]);
+
+  const handleGroupingChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const { target } = event;
+    const grouping = target.value as Grouping;
+    orderBookRef.current.updateGrouping(grouping);
+  };
+
   return (
     <div className="container mx-auto">
       <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">WS Order Book</h2>
+      <select onChange={handleGroupingChange}>
+        {Object.entries(Grouping).map(([name, value]) => {
+          return (
+            <option key={name} value={value}>
+              {name}
+            </option>
+          );
+        })}
+      </select>
       <div className="flex">
         <ul>
           {bids.map((bid) => {
